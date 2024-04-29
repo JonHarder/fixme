61417606b37c369e2fc90c31734e9ebdc4bb24fe
Jon <jharder@kipsu.com>
2024-04-29 11:13:35 -0500

    Utilize Config::get_fixme_mut in commands::fix::fix
    <!-- ps-id: 6bc8e89a-ef53-4ccb-b573-1ce11b50adb5 -->

--- src/commands/fix.rs
+++ src/commands/fix.rs
@@ -1,18 +1,30 @@
-use crate::config::Config;
 use crate::config::FixId;
+use crate::config::{Config, Fixme};
 
-pub fn fix(conf: &mut Config, id: FixId) -> std::io::Result<()> {
-    let fixme = conf
-        .projects
-        .get_mut(id.project_id)
-        .and_then(|p| p.get_fixme_mut(id.fixme_id))
-        .ok_or_else(|| {
-            std::io::Error::new(
-                std::io::ErrorKind::InvalidInput,
-                "Invalid project or fixme id. See 'fixme list'",
-            )
-        })?;
+pub fn fix(conf: &mut Config, id: FixId) -> Result<&Fixme, std::io::Error> {
+    let fixme: &mut Fixme = conf.get_fixme_mut(id)?;
     fixme.complete();
     conf.save()?;
-    Ok(())
+    let fixme = conf
+        .projects
+        .get(id.project_id)
+        .and_then(|p| p.get_fixme(id.fixme_id));
+    // This should be fine since we've already done bounds checking above and returned
+    // Err if the FixId was invalid.
+    Ok(fixme.unwrap())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_out_of_bounds_fix_id_is_error() {
+        let mut conf = crate::config::Config::new();
+        let fix_id = FixId {
+            project_id: 0,
+            fixme_id: 0,
+        };
+        assert!(fix(&mut conf, fix_id).is_err_and(|e| e.kind() == std::io::ErrorKind::InvalidInput));
+    }
 }
